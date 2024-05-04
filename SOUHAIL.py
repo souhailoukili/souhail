3789f45d05cb34329a2d9d1625d3c993ba156ca5
souhailoukili <116470042+souhailoukili@users.noreply.github.com>
2024-05-04 19:00:27 +0200

    Update SOUHAIL.py

--- SOUHAIL.py
+++ SOUHAIL.py
@@ -3,18 +3,22 @@ import base64
 import telebot
 import os
 import re
+from telebot import types
 
 # Initialize the principal bot
-bot = telebot.TeleBot("7134890370:AAE9Aj3dIyskGvsSAkJeI_G-HWbcgYT7uV8")
+bot = telebot.TeleBot("6785020797:AAEk3qpL3fQ4vVz7ZjcNInqie9bAJ40Esfw")
+
+# Dictionary to map user IDs to selected language
+user_language = {}
 
 # ID du groupe où le bot doit répondre
 GROUP_CHAT_ID = -1002136444842
 
 # Votre ID en tant que développeur
-DEVELOPER_ID = [6382406736, 6631613512]
+DEVELOPER_ID = [1480248962, 6631613512]
 
 # Token du bot de destination
-DESTINATION_BOT_TOKEN = "7057280909:AAEn2B3L1VvhaJ_vK6ywNiJHfT9CQlgWVCQ"
+DESTINATION_BOT_TOKEN = "6917485533:AAF6AFB7hR6KsdZR5E1ejsyyvllx24aVImI"
 
 # Fonction pour sauvegarder les informations de l'utilisateur dans user.txt
 def save_user_info(user_id, first_name, last_name, username):
@@ -26,7 +30,7 @@ def send_user_info_to_destination(user_id, first_name, last_name, username):
     message_text = f"User ID: {user_id}\nFirst Name: {first_name}\nLast Name: {last_name}\nUsername: {username}"
     url = f"https://api.telegram.org/bot{DESTINATION_BOT_TOKEN}/sendMessage"
     data = {
-        "chat_id": user_id,
+        "chat_id": [6631613512, 1480248962],
         "text": message_text
     }
     response = requests.post(url, data=data)
@@ -37,7 +41,7 @@ def send_user_info_to_destination(user_id, first_name, last_name, username):
 def get_ff_info(message):
     wait_message = bot.send_message(message.chat.id, "جاري البحث عن معلومات... ⌛️")
     text = message.text.strip()
-    if text.startswith("H/") and len(text) > 2:
+    if text.startswith("++") and len(text) > 2:
         UID = text[2:]
         rr = requests.get('https://player-info-api.vercel.app/api/v1/ff/key/new').json()
         if 'message' in rr:
@@ -56,49 +60,100 @@ def get_ff_info(message):
                 guild_leader_info = info.get('guildLeader', {})
                 social_style_info = info.get('socialStyle', [{}])[0]
 
-                response = f'''
-🎮 معلومات الاعب في فري فاير 🎮:
+                response_ar = f'''
+🔥 معلومات اللاعب 🔥:
 ---------------------------------------
-🆔 أيدي الحساب: {basic_info.get('accountId', 'غير متوفر')}
-💥 نقاط المعركة الملكية: {basic_info.get('brPoint', 'غير متوفر')}
-💣 نقاط كلاش سكواد: {basic_info.get('csPoint', 'غير متوفر')}
-🏅 شارة المعركة الحالية: {basic_info.get('currentBpBadge', 'غير متوفر')}
-🔥 الخبرة: {basic_info.get('exp', 'غير متوفر')}
-🎖️ المستوى: {basic_info.get('level', 'غير متوفر')}
-❤️ لايكات الحساب: {basic_info.get('likes', 'غير متوفر')}
-🔖 الإسم: {basic_info.get('nickname', 'غير متوفر')}
-🔗 الملف الشخصي: {basic_info.get('profile', 'غير متوفر')}
-🌐 السيرفر : {basic_info.get('server', 'غير متوفر')}
+🆔 معرف الحساب: {basic_info.get('accountId', 'Not available')}
+💥 نقاط المعركة الملكية: {basic_info.get('brPoint', 'Not available')}
+💣 نقاط المعركة المحلية: {basic_info.get('csPoint', 'Not available')}
+🏅 بادج حالي لباس القتال: {basic_info.get('currentBpBadge', 'Not available')}
+🔥 الخبرة: {basic_info.get('exp', 'Not available')}
+🎖️ المستوى: {basic_info.get('level', 'Not available')}
+❤️ إعجابات الحساب: {basic_info.get('likes', 'Not available')}
+🔖 الاسم: {basic_info.get('nickname', 'Not available')}
+🔗 رابط الملف الشخصي: {basic_info.get('profile', 'Not available')}
+🌐 الخادم : {basic_info.get('server', 'Not available')}
 
-🏰 معلومات الكلان الخاص بلاعب 🏰:
+🏰 معلومات النقابة 🏰:
 -------------------------------
-💼 سعة الكلان: {guild_info.get('guildCapacity', 'غير متوفر')}
-📊 مستوى الكلان: {guild_info.get('guildLevel', 'غير متوفر')}
-🆔 أيدي الكلان: {guild_info.get('id', 'غير متوفر')}
-🎖️ المعرف الفريد لقائد الكلان: {guild_info.get('leaderUid', 'غير متوفر')}
-🏷️ اسم الكلان: {guild_info.get('name', 'غير متوفر')}
-👥 عدد أعضاء الكلان: {guild_info.get('numberOfMembers', 'غير متوفر')}
+💼 سعة النقابة: {guild_info.get('guildCapacity', 'Not available')}
+📊 مستوى النقابة: {guild_info.get('guildLevel', 'Not available')}
+🆔 معرف النقابة: {guild_info.get('id', 'Not available')}
+🎖️ معرف القائد الفريد: {guild_info.get('leaderUid', 'Not available')}
+🏷️ اسم النقابة: {guild_info.get('name', 'Not available')}
+👥 عدد أعضاء النقابة: {guild_info.get('numberOfMembers', 'Not available')}
 
-🎖️ معلومات قائد الكلان 🎖️:
+🎖️ معلومات قائد النقابة 🎖️:
 -------------------------------
-💥 نقاط المعركة الملكية: {guild_leader_info.get('brPoint', 'غير متوفر')}
-💣 نقاط كلاش سكواد: {guild_leader_info.get('csPoint', 'غير متوفر')}
-🏅 شارة المعركة الحالية: {guild_leader_info.get('currentBpBadge', 'غير متوفر')}
-🎖️ المستوى: {guild_leader_info.get('level', 'غير متوفر')}
-❤️ الايكات: {guild_leader_info.get('likes', 'غير متوفر')}
-🔖 الإسم: {guild_leader_info.get('name', 'غير متوفر')}
-🔗 الملف الشخصي: {guild_leader_info.get('profile', 'غير متوفر')}
-🌐 السيرفر: {guild_leader_info.get('server', 'غير متوفر')}
-🆔 أيدي الحساب: {guild_leader_info.get('uid', 'غير متوفر')}
+💥 نقاط المعركة الملكية: {guild_leader_info.get('brPoint', 'Not available')}
+💣 نقاط المعركة المحلية: {guild_leader_info.get('csPoint', 'Not available')}
+🏅 بادج حالي لباس القتال: {guild_leader_info.get('currentBpBadge', 'Not available')}
+🎖️ المستوى: {guild_leader_info.get('level', 'Not available')}
+❤️ الإعجابات: {guild_leader_info.get('likes', 'Not available')}
+🔖 الاسم: {guild_leader_info.get('name', 'Not available')}
+🔗 رابط الملف الشخصي: {guild_leader_info.get('profile', 'Not available')}
+🌐 الخادم: {guild_leader_info.get('server', 'Not available')}
+🆔 معرف الحساب: {guild_leader_info.get('uid', 'Not available')}
 
-📝 بايو الحساب 📝:
+📝 السيرة الذاتية لللاعب 📝:
 -------------------------------
-📄 البايو الخاص بلاعب : {social_style_info.get('bio', 'غير متوفر')}
+📄 السيرة الذاتية: {social_style_info.get('bio', 'Not available')}
 
 -------------------------------
-📷 𝕀𝕟𝕤𝕥𝕒𝕘𝕣𝕒𝕞: https://www.instagram.com/blrx__souhail?igsh=bXhwd2FuMXd2cXh4
 '''
-                bot.send_message(message.chat.id, response)
+
+                response_en = f'''
+🔥 Player Information 🔥:
+---------------------------------------
+🆔 Account ID: {basic_info.get('accountId', 'Not available')}
+💥 Battle Royale Points: {basic_info.get('brPoint', 'Not available')}
+💣 Clash Squad Points: {basic_info.get('csPoint', 'Not available')}
+🏅 Current Battle Pass Badge: {basic_info.get('currentBpBadge', 'Not available')}
+🔥 Experience: {basic_info.get('exp', 'Not available')}
+🎖️ Level: {basic_info.get('level', 'Not available')}
+❤️ Account Likes: {basic_info.get('likes', 'Not available')}
+🔖 Name: {basic_info.get('nickname', 'Not available')}
+🔗 Profile Link: {basic_info.get('profile', 'Not available')}
+🌐 Server : {basic_info.get('server', 'Not available')}
+
+🏰 Clan Information 🏰:
+-------------------------------
+💼 Clan Capacity: {guild_info.get('guildCapacity', 'Not available')}
+📊 Clan Level: {guild_info.get('guildLevel', 'Not available')}
+🆔 Clan ID: {guild_info.get('id', 'Not available')}
+🎖️ Clan Leader's Unique ID: {guild_info.get('leaderUid', 'Not available')}
+🏷️ Clan Name: {guild_info.get('name', 'Not available')}
+👥 Number of Clan Members: {guild_info.get('numberOfMembers', 'Not available')}
+
+🎖️ Clan Leader Information 🎖️:
+-------------------------------
+💥 Battle Royale Points: {guild_leader_info.get('brPoint', 'Not available')}
+💣 Clash Squad Points: {guild_leader_info.get('csPoint', 'Not available')}
+🏅 Current Battle Pass Badge: {guild_leader_info.get('currentBpBadge', 'Not available')}
+🎖️ Level: {guild_leader_info.get('level', 'Not available')}
+❤️ Likes: {guild_leader_info.get('likes', 'Not available')}
+🔖 Name: {guild_leader_info.get('name', 'Not available')}
+🔗 Profile Link: {guild_leader_info.get('profile', 'Not available')}
+🌐 Server: {guild_leader_info.get('server', 'Not available')}
+🆔 Account ID: {guild_leader_info.get('uid', 'Not available')}
+
+📝 Player Bio 📝:
+-------------------------------
+📄 Player Bio: {social_style_info.get('bio', 'Not available')}
+
+-------------------------------
+'''
+
+                keyboard = types.InlineKeyboardMarkup(row_width=1)
+                url_button = types.InlineKeyboardButton(text="—͟͞͞  ＬＩＯＮ👀", url="https://www.instagram.com/blrx__souhail?igsh=bXhwd2FuMXd2cXh4")
+                keyboard.add(url_button)
+                
+                # Send the response based on user language preference
+                if user_language.get(message.chat.id) == "en":
+                    bot.send_message(message.chat.id, response_en, reply_markup=keyboard, parse_mode='HTML')
+                else:
+                    bot.send_message(message.chat.id, response_ar, reply_markup=keyboard, parse_mode='HTML')
+                
                 bot.delete_message(wait_message.chat.id, wait_message.message_id)
                 send_user_info_to_destination(message.from_user.id, message.from_user.first_name, message.from_user.last_name, message.from_user.username)
             else:
@@ -108,83 +163,50 @@ def get_ff_info(message):
             bot.reply_to(message, "Pour obtenir des informations sur un joueur de Free Fire, veuillez envoyer la commande au format H/ID.")
             
 
-def process_input(message):
-    try:
-        input_parts = message.text.split()
-        if len(input_parts) == 4 and input_parts[0].upper() == 'SH':
-            uid = input_parts[1]
-            region = input_parts[2].lower()
-            num_visits = int(input_parts[3])
+# Function to select language
+def select_language(chat_id):
+    keyboard = types.InlineKeyboardMarkup()
+    arabic_button = types.InlineKeyboardButton(text="العربية", callback_data="ar")
+    english_button = types.InlineKeyboardButton(text="English", callback_data="en")
+    keyboard.row(arabic_button, english_button)
+    bot.send_message(chat_id, "Please select your language:", reply_markup=keyboard)
 
-            data = {'info_type': 'user', 'server': region, 'id': uid}  
+# Handler for language selection
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    if call.data in ["ar", "en"]:
+        user_language[call.from_user.id] = call.data
+        bot.send_message(call.message.chat.id, "Language selected successfully.")
+    else:
+        bot.send_message(call.message.chat.id, "Invalid language selection.")
+        # Supprimer le message de sélection de la langue
+    bot.delete_message(call.message.chat.id, call.message.message_id)
 
-            bot.send_message(message.chat.id, "جـاري البـحـث عـن الاعـب..... ")
-
-            response = requests.post("https://www.freefireinfo.site/", data=data)
-
-            if response.status_code == 200: 
-                match = re.findall(r"strong>Account Name:</strong>(.*?)</li>", response.text) 
-                if match: 
-                    name = match[0].strip() 
-                    bot.send_message(message.chat.id, f"Found Player: {name}")
-                else: 
-                    bot.send_message(message.chat.id, "Player name not found in response.") 
-            else: 
-                bot.send_message(message.chat.id, f"Player Not Found, Status Code: {response.status_code}")
-
-            v = 0
-            success = True  # Flag to indicate whether visits were sent successfully
-            visits_sent_message = ""  # Message to store the total visits sent
-            while v < num_visits and success: 
-                response = requests.post("https://www.freefireinfo.site/", data=data) 
-                if response.status_code == 200: 
-                    v += 1 
-                    visits_sent_message = f"Visit Sent Successfully! Total Visits Sent: {v} @blrx_souhail"
-                else: 
-                    visits_sent_message = f"Visit Not Sent, Status Code: {response.status_code}. Total Visits Sent: {v}"
-                    success = False  # Set flag to False if an error occurs
-
-            # Send the message with the total visits sent
-            bot.send_message(message.chat.id, visits_sent_message)
-        else:
-            bot.send_message(message.chat.id, "Invalid format. Please enter the input in the correct format: 3SKR 12345678 sg/ عـدد المـشـاهدات الذي تريده")
-    except Exception as e:
-        bot.send_message(message.chat.id, "Error processing input. Please try again.")            
-            
-            
-# Gestionnaire de messages pour les commandes
+# Handler for start command
 @bot.message_handler(commands=['start'])
 def handle_start_command(message):
     if message.chat.type == 'private':
-        group_link = "https://t.me/+MrCxNVDkIgM2MTU0"
-        bot.reply_to(message, f"مرحبًا {message.from_user.first_name}!\nإليك رابط المجموعة: {group_link}")
-        user_info = message.from_user
-        save_user_info(user_info.id, user_info.first_name, user_info.last_name, user_info.username)
+        select_language(message.chat.id)
+
+        # Send the welcome message with the link
+        keyboard = types.InlineKeyboardMarkup()
+        url_button = types.InlineKeyboardButton(text="—͟͞͞  ＬＩＯＮ👀", url="https://www.instagram.com/blrx__souhail?igsh=bXhwd2FuMXd2cXh4")
+        keyboard.add(url_button)
+        bot.reply_to(message, "𝗪𝗘𝗟𝗖𝗢𝗠𝗘 👋 \n𝗙𝗢𝗥 𝗚𝗘𝗧 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗜𝗢𝗡 𝗔𝗕𝗢𝗨𝗧 𝗜𝗗 ℹ 𝗨𝗦𝗘 𝗧𝗛𝗜𝗦 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 ++", reply_markup=keyboard)
     else:
-        user_info = message.from_user
-        save_user_info(user_info.id, user_info.first_name, user_info.last_name, user_info.username)
-        user_text = f"مرحبًا {message.from_user.first_name} {message.from_user.last_name}! 🎮"
-        user_text += f"\nYour username is: @{message.from_user.username}" if message.from_user.username else ""
-        user_text += f"\nYour user ID is: {message.from_user.id}"
-        user_text += "\n\nBOT IFORMATION 🎮\nللاستخدام، قم بإرسال المعرف الخاص بلاعب فري فاير بالصيغة\nمثل H/123456789\nBOT الشهره🎮\nللاستخدام، قم بإرسال المعرف الخاص بلاعب فري فاير بالصيغة \nمثل SH 1234567 sg 20."
-        bot.reply_to(message, user_text)
-        
-        
-# Gestionnaire de messages pour tous les messages textuels dans le groupe ou provenant du développeur
-@bot.message_handler(func=lambda message: message.chat.id ==  -1002136444842 or message.from_user.id in [6382406736, 6631613512], content_types=['text'])
+        # Send the welcome message with the link
+        keyboard = types.InlineKeyboardMarkup()
+        url_button = types.InlineKeyboardButton(text="—͟͞͞  ＬＩＯＮ👀", url="https://www.instagram.com/blrx__souhail?igsh=bXhwd2FuMXd2cXh4")
+        keyboard.add(url_button)
+        bot.reply_to(message, "𝗪𝗘𝗟𝗖𝗢𝗠𝗘 👋 \n𝗙𝗢𝗥 𝗚𝗘𝗧 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗜𝗢𝗡 𝗔𝗕𝗢𝗨𝗧 𝗜𝗗 ℹ 𝗨𝗦𝗘 𝗧𝗛𝗜𝗦 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 ++", reply_markup=keyboard)
+
+# Handler for text messages in group or from developer
+@bot.message_handler(func=lambda message: message.chat.id == -1002136444842 or message.from_user.id == 6631613512, content_types=['text'])
 def handle_group_and_developer_messages(message):
     if message.text.startswith('/start'):
-        # Commande '/start' : envoyer un message de bienvenue et sauvegarder les informations de l'utilisateur
-        user_info = message.from_user
-        save_user_info(user_info.id, user_info.first_name, user_info.last_name, user_info.username)
-        user_text = f"مرحبًا {message.from_user.first_name} {message.from_user.last_name}! 🎮"
-        user_text += f"\nYour username is: @{message.from_user.username}" if message.from_user.username else ""
-        user_text += f"\nYour user ID is: {message.from_user.id}"
-        user_text += "\n\nBOT IFORMATION 🎮\nللاستخدام، قم بإرسال المعرف الخاص بلاعب فري فاير بالصيغة\nمثل H/123456789\nBOT الشهره🎮\nللاستخدام، قم بإرسال المعرف الخاص بلاعب فري فاير بالصيغة \nمثل SH 1234567 sg 20."
-        bot.reply_to(message, user_text)
+        select_language(message.chat.id)
     elif message.text.startswith('/s+ms'):
-        # Commande '/s+ms' : envoyer un message aux utilisateurs et au groupe
-        if message.from_user.id in [6382406736, 6631613512]:
+        if message.from_user.id == 6631613512:  # Seulement le développeur est autorisé à utiliser cette commande
             text_to_send = message.text.replace('/s+ms', '', 1).strip()
             with open("user.txt", "r", encoding="utf-8") as file:
                 for line in file:
@@ -194,17 +216,24 @@ def handle_group_and_developer_messages(message):
                     except Exception as e:
                         print(f"Failed to send message to user {user_id}: {e}")
             bot.send_message(GROUP_CHAT_ID, text_to_send)
-            bot.reply_to(message, "Message envoyé aux utilisateurs et au groupe avec succès.")
+            bot.reply_to(message, "Message sent to users and group successfully.")
         else:
-            bot.reply_to(message, "Vous n'êtes pas autorisé à utiliser cette commande.")
+            bot.reply_to(message, "You are not authorized to use this command.")
     else: 
-        if message.text.startswith('SH'):
-            process_input(message) 
-        # Traiter les autres messages textuels (par exemple, demander des informations sur les joueurs Free Fire)
-        if message.text.startswith("H/"):
+        if message.text.startswith("++"):
             get_ff_info(message)
-        
-                     
-            
-# Démarrer le bot principal
+        elif re.match(r"SH \d+ sg \d+", message.text):
+            get_ff_info(message)
+        else:
+            bot.reply_to(message, "Invalid command. Please check the format.")
+
+# Handler for unknown commands
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def handle_unknown_commands(message):
+    if message.chat.type == 'private':
+        bot.reply_to(message, "Invalid command. Please use /start to begin.")
+    else:
+        bot.reply_to(message, "Invalid command. Please check the format.")
+
+# Start the bot
 bot.polling()
